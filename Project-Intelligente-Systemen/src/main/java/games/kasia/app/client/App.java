8be17ba8a94ffda6114cda49df243d8ee2f5ed67
yuna <koningkaas05@gmail.com>
2026-09-08 12:56:50 +0200

    structuur voor main met mvn (documentatie over hoe je het gebruikt komt straks)

---
+++ Project-Intelligente-Systemen/src/main/java/games/kasia/app/client/App.java
@@ -0,0 +1,13 @@
+package games.kasia.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
