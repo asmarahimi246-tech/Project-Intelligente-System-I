b26554543e0be414def8f742bb39d6a2104f9d52
Rien899 <r.breider@st.hanze.nl>
2026-09-17 17:45:24 +0200

    test

--- Project tictactoe/src/Main.java
+++ Project tictactoe/src/Main.java
@@ -1,4 +1,15 @@
 package PACKAGE_NAME;
 
-public class main {
+        Client client = new Client();
+        ServerListener listener = new ServerListener(client);
+
+        client.sendCommand("login " + playername);
+        client.sendCommand("subscribe tic-tac-toe");
+
+        String message;
+        while ((message = client.getReader().readLine()) != null) {
+            listener.Commandhandler(message);
+            System.out.println("SERVER: " + message);
+        }
+    }
 }
