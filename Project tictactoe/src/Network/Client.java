5e2c70e59b0c82c097a559d12365c18a242218fe
Rien899 <r.breider@st.hanze.nl>
2026-09-17 15:20:24 +0200

    test

---
+++ Project tictactoe/src/Network/Client.java
@@ -0,0 +1,16 @@
+package framework;
+
+import java.io.*;
+import java.net.*;
+
+public class client {
+    private Socket socket;
+    private BufferedReader reader;
+    private PrintWriter writer;
+
+    public client() throws IOException {
+        socket = new Socket("localhost", 7789);
+        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        writer = new PrintWriter(socket.getOutputStream(), true);
+    }
+}
