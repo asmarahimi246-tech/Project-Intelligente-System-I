6788481a0f66dc53e586e3bc4f9f830752ccd0a1
Andy7920 <a.zhang.2@st.hanze.nl>
2026-09-17 22:21:38 +0200

    Check for a winner

--- prototype.java
+++ prototype.java
@@ -7,23 +7,36 @@ public class prototype {
 
         Scanner input = new Scanner(System.in);
         Random computer = new Random();
+
+        while (true) {
         
-        printBoard(board);
+            printBoard(board);
 
-        System.out.print("Choose a position (1-9): ");
-        int position = input.nextInt();
+            System.out.print("Choose a position (1-9): ");
+            int position = input.nextInt();
 
-        board[position - 1] = 'X'; // position - 1 because the indexes are 0-8
+            board[position - 1] = 'X'; // position - 1 because the indexes are 0-8
 
-        int computerPosition = computer.nextInt(9); // random number from 0-8
+            if (hasWon(board, "X")) {
+                printBoard(board);
+                System.out.println("You win!");
+                break;
+            }
 
-        while (board[computerPosition] == 'X' || board[computerPosition] == 'O') {
-            computerPosition = computer.nextInt(9);
+            int computerPosition = computer.nextInt(9); // random number from 0-8
+
+            while (board[computerPosition] == 'X' || board[computerPosition] == 'O') {
+                computerPosition = computer.nextInt(9);
+            }
+
+            board[computerPosition] = 'O';
+
+            if (hasWon(board, "O")) {
+                printBoard(board);
+                System.out.println("The computer wins!");
+                break;
+            }
         }
-
-        board[computerPosition] = 'O';
-
-        printBoard(board);
     }    
 
     static void printBoard(char[] board) {
@@ -33,5 +46,38 @@ public class prototype {
         System.out.println("----------");
         System.out.println(board[6] + " | " + board[7] + " | " + board[8]);
 
+    }
+
+    static boolean hasWon(char[] board, char player) {
+
+        // Horizontal
+        if (board[0] == player && board[1] == player && board[2] == player) {
+            return true;
+        }
+        if (board[3] == player && board[4] == player && board[5] == player) {
+            return true;
+        }        
+        if (board[6] == player && board[7] == player && board[8] == player) {
+            return true;
+        }
+        // Diagonal
+        if (board[0] == player && board[4] == player && board[8] == player) {
+            return true;
+        }
+        if (board[6] == player && board[4] == player && board[2] == player) {
+            return true;
+        }        
+        // Vertical
+        if (board[0] == player && board[3] == player && board[6] == player) {
+            return true;
+        }
+        if (board[1] == player && board[4] == player && board[7] == player) {
+            return true;
+        }
+        if (board[2] == player && board[5] == player && board[8] == player) {
+            return true;
+        }        
+
+        return false;
     }
 }
