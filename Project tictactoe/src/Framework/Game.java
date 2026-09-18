55a22df0938edc41cce8aa9ca41a73804e7075ef
Rien899 <r.breider@st.hanze.nl>
2026-09-18 16:31:26 +0200

    test

--- Project tictactoe/src/Framework/Game.java
+++ Project tictactoe/src/Framework/Game.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Game {
     private Client client;
     private char mySymbol = 'X';
-    private char[] board = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    private char[] board = {'1','2','3','4','5','6','7','8','9'};
 
     public Game(Client client) {
         this.client = client;
