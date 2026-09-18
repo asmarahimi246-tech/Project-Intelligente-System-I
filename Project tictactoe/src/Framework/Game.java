8c64f9c778ee9b6461149359c03dc41688ca22a0
Rien899 <r.breider@st.hanze.nl>
2026-09-18 14:23:54 +0200

    test

--- Project tictactoe/src/Framework/Game.java
+++ Project tictactoe/src/Framework/Game.java
@@ -2,3 +2,28 @@ package Game;
 
 import Network.Client;
 
+        if (board[number] != 'X' && board[number] != 'O') {
+            board[number] = 'X';
+            System.out.println("Tussenstand");
+            printBoard();
+            client.sendCommand("move " + number);
+        }
+    }
+
+    public void Move(int number) {
+        if (board[number] != 'X') {
+            board[number] = 'O';
+            printBoard();
+        }
+    }
+
+    public void printBoard() {
+        System.out.println("|---|---|---|");
+        System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+        System.out.println("|---|---|---|");
+    }
+}
