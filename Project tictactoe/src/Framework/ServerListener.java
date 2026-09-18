403d6f14c0988d147adeacf05281b33ec19d8da0
Rien899 <r.breider@st.hanze.nl>
2026-09-18 13:57:04 +0200

    test

--- Project tictactoe/src/Framework/ServerListener.java
+++ Project tictactoe/src/Framework/ServerListener.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class ServerListener {
     private Client client;
 
+    static char[] board = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
     public ServerListener(Client client) {
         this.client = client;
     }
