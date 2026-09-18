09572a69314dacb921463f73e33e9b5549b767ec
Rien899 <r.breider@st.hanze.nl>
2026-09-18 15:34:21 +0200

    test

--- Project tictactoe/src/Framework/ServerListener.java
+++ Project tictactoe/src/Framework/ServerListener.java
@@ -35,14 +35,5 @@ public class ServerListener {
             }
         }
     }
-
-    static void printBoard() {
-        System.out.println("|---|---|---|");
-        System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
-        System.out.println("|---|---|---|");
-    }
 }
+
