fc8556236a355fc0e67eea87d3832cf8d09c0b95
Rien899 <r.breider@st.hanze.nl>
2026-09-18 16:24:03 +0200

    test

--- Project tictactoe/src/Framework/ServerListener.java
+++ Project tictactoe/src/Framework/ServerListener.java
@@ -16,7 +16,7 @@ public class ServerListener {
         System.out.println(message);
 
         if (message.startsWith("SVR GAME YOURTURN")) {
-            hasHadTurn = true;
+            hadturn = true;
             game.Turn();
         }
 
