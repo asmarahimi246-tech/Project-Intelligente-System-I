60c16e7a6fa74549bf6b4e277f3ce30052164198
Rien899 <r.breider@st.hanze.nl>
2026-09-18 15:33:46 +0200

    test

--- Project tictactoe/src/Main.java
+++ Project tictactoe/src/Main.java
@@ -2,10 +2,6 @@ import Framework.Game;
 import Network.Client;
 import java.io.IOException;
 import Framework.ServerListener;
-import java.util.Scanner;
-import java.util.Random;
-import Framework.Game;
-
 
 public class Main {
     public static void main(String[] args) throws IOException {
